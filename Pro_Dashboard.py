c0c296f0a9d9bcd542d2cddf46f103623e5ca284
WiseGeorgie <65507416+Jorgito58@users.noreply.github.com>
2023-03-03 00:14:37 -0500

    Update Pro_Dashboard.py

--- Pro_Dashboard.py
+++ Pro_Dashboard.py
@@ -9,7 +9,7 @@ from streamlit_option_menu import option_menu
 import time
 
 #Loading Model
-model_new = keras.models.load_model('saved_model/Best Digit Recognizer Model/')
+model_new = keras.models.load_model('saved_model/Best Digit Recognizer Model/Digit_recognizer.h5')
 
 
 
